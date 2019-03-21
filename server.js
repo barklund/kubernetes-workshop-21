5c3c06259951e1cf1a105d01664a2665f609ad5c
Morten Barklund <morten@barklund.dk>
2019-03-21 12:37:49 +0100

    feature: update code

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express'
 const app = express()
 
 app.get('/', (req, res ) => {
-  res.send('Hello March 21 Once Again!')
+  res.send('Hello March 21 For the last time (maybe)!')
 })
 
 const PORT = process.env.PORT || 9000
